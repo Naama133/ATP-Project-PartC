678f23506436a512ceb021308e24b36582153190
Darabu273 <dAr8194765>
2021-05-31 20:30:21 +0300

    ExitController

--- src/View/ExitController.java
+++ src/View/ExitController.java
@@ -1,22 +1,31 @@
 package View;
 
+import ViewModel.MyViewModel;
+import com.sun.glass.ui.View;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ExitController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        //todo
     }
 
     public void continueExit(ActionEvent actionEvent) {
-
+        MyViewModel viewModel = MyViewModel.getInstance();
+        viewModel.exitGame();
+        System.exit(0);
     }
 
     public void abortExit(ActionEvent actionEvent) {
-
+       //todo
     }
 }
